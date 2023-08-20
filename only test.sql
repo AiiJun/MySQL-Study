8e2b4ba9aeb7d300134d21e2ae83d1d123c7d178
AiiJun <137049458+AiiJun@users.noreply.github.com>
2023-08-20 12:49:19 +0800

    视图的案例

--- only test.sql
+++ only test.sql
@@ -526,14 +526,20 @@ INSERT into stu_v_count values(10);
 
 
 
+-- 案例：
+-- 1.为了保证数据库表的安全性，开发人员在操作tb_user表时，只能看到的用户的基本字段，屏蔽手机号和邮箱两个字段
+CREATE view tb_user_view as SELECT id,name,job from tb_user;
+
+SELECT * from tb_user_view;
 
 
 
+-- 2.查询每个学生所选修的课程（三张表联查），这个功能在很多业务中都有使用到，为了简化操作，定义视图
+SELECT s.name,s.no,c.name from student s,student_course sc,course c where s.id = sc.studentid and sc.courseid = c.id;
 
+create view stu_course_view as SELECT s.name student_name,s.no student_no,c.name course_name from student s,student_course sc,course c where s.id = sc.studentid and sc.courseid = c.id;
 
-
-
-
+SELECT * FROM stu_course_view;
 
 
 
